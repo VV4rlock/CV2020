1853805c2e4a487b1cc8dbce2047c17259f9ddd4
warlock <Vlad9n>
2020-05-06 01:10:16 +0300

    final

--- object_localization.py
+++ object_localization.py
@@ -1,9 +1,10 @@
 from config import *
 from matplotlib import pyplot as plt
+import sklearn.linear_model as lm
 
 OBJ_PATH = r'box.png'
 SCENE_PATH = r'box_in_scene.png'
-
+DRAW_VECTORS = False
 
 class Localizator():
     def __init__(self):
@@ -33,6 +34,7 @@ class Localizator():
         max_len = 0
         max_features = 0
         max_keypoints = None
+        more_than3 = set()
         print(scene_H, scene_W)
         for match in matches:
             obj_kp = kp1[match.queryIdx]
@@ -40,19 +42,22 @@ class Localizator():
             delta_theta = scene_kp.angle - obj_kp.angle
             scale = scene_kp.octave - obj_kp.octave
             center_vec = obj_center[0] - obj_kp.pt[0], obj_center[1] - obj_kp.pt[1]
-            cv2.line(object, (int(obj_kp.pt[0]), int(obj_kp.pt[1])),
+            if DRAW_VECTORS:
+                cv2.line(object, (int(obj_kp.pt[0]), int(obj_kp.pt[1])),
                      (int(obj_kp.pt[0] + center_vec[0]), int(obj_kp.pt[1] + center_vec[1])), 255, 1)
+
             center_vec = (
                 np.array(center_vec).dot(
                     cv2.getRotationMatrix2D(center=(0, 0), angle=delta_theta,
                                             scale=(self.scale_factor ** scale))[:, :2]))
-            cv2.line(scene, (int(scene_kp.pt[0]), int(scene_kp.pt[1])),
-                     (int(scene_kp.pt[0] + center_vec[0]), int(scene_kp.pt[1] + center_vec[1])), 255, 1)
+            if DRAW_VECTORS:
+                cv2.line(scene, (int(scene_kp.pt[0]), int(scene_kp.pt[1])),
+                        (int(scene_kp.pt[0] + center_vec[0]), int(scene_kp.pt[1] + center_vec[1])), 255, 1)
 
 
             features = (scale, int(delta_theta // self.angle_step), int((scene_kp.pt[1] + center_vec[1]) // self.scene_y_step),
                         int((scene_kp.pt[0] + center_vec[0]) // self.scene_x_step))
-            print(features)
+
             try:
                 curr_list = accum[features[0]][features[1]][features[2]][features[3]]
                 # noinspection PyTypeChecker
@@ -61,6 +66,8 @@ class Localizator():
                     max_keypoints = curr_list
                     max_features = features
                     max_len = len(curr_list)
+                if len(curr_list) > 2:
+                    more_than3.add(features)
             except IndexError as e:
                 continue # bad match?? point + center_vec
 
@@ -70,14 +77,54 @@ class Localizator():
         rect_new = rect_kp.dot(cv2.getRotationMatrix2D(center=(0, 0), angle=(max_features[1]*self.angle_step + self.angle_step/2),
                                             scale=(self.scale_factor ** max_features[0]))[:, :2])
         rect_new = np.int0(rect_new + center)
-        
+
+        #scene = cv2.cvtColor(scene, cv2.COLOR_GRAY2BGR)
+        #cv2.drawContours(scene, [rect_new], 0, (0, 255, 0), 2)
+
+        #img = cv2.drawMatches(object, kp1, scene, kp2, matches[:4], None)
+        #plt.imshow(img), plt.show()
+
+        kp_dict = {f: accum[f[0]][f[1]][f[2]][f[3]] for f in more_than3}
+        skm = lm.LinearRegression(fit_intercept=False)
+        max_size = 4
+        x = None
+        while True:
+            kps = sum([kp_dict[f] for f in kp_dict if len(kp_dict[f]) > 2], [])
+            print(len(kps))
+            A, b = [], []
+            for obj_kp, scene_kp in kps:
+                A.append([obj_kp.pt[0], obj_kp.pt[1], 0, 0, 1, 0])
+                A.append([0, 0, obj_kp.pt[0], obj_kp.pt[1], 0, 1])
+                b.append(scene_kp.pt)
+            A = np.array(A)
+            b = np.array(b).reshape(-1, 1)
+            skm.fit(A, b)
+            x = skm.coef_.reshape((-1, 1))
+            indices = np.where(np.any((A.dot(x) - b > max_size / 4).reshape(-1, 2), axis=1))[0]
+            if indices.size == 0:
+                break
+            delete_candidates = [kps[i] for i in indices]
+            for key in kp_dict:  # also walk on list with size=2
+                for kp in kp_dict[key]:
+                    if kp in delete_candidates:
+                        kp_dict[key].remove(kp)
+
+
+        rect_kp = np.array([(0, 0), (obj_W, 0), (obj_W, obj_H),
+                            (0, obj_H)])
+        rect_new = np.int0(rect_kp.dot((x[:4].reshape((2, 2)).T)) + x[4:].reshape((1, 2)))
+        print(x, rect_new, rect_kp)
         scene = cv2.cvtColor(scene, cv2.COLOR_GRAY2BGR)
         cv2.drawContours(scene, [rect_new], 0, (0, 255, 0), 2)
-
-
-
-        img = cv2.drawMatches(object, kp1, scene, kp2, matches, None)
+        img = cv2.drawMatches(object, kp1, scene, kp2, matches[:4], None)
         plt.imshow(img), plt.show()
+
+
+
+
+
+
+
 
 
 
