7e29f21f3ec346419ecc28c1161ed8bd6b636a18
warlock <Vlad9n>
2020-05-05 21:49:08 +0300

    classification_and_localization

--- ORB_classification.py
+++ ORB_classification.py
@@ -2,34 +2,46 @@ from config import *
 from ORB import ORB
 import re
 from sklearn_extra.cluster import KMedoids
+from sklearn.cluster import KMeans
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
-import torchvision
-import torchvision.transforms as transforms
-from torchvision.utils import make_grid
+def term_frequency_encoding(descriptors, claster_obj):
+    descriptors = np.unpackbits(descriptors, axis=1)
+    dintandces = claster_obj.transform(descriptors)
+    indices = np.argmin(dintandces, axis=1)
+    clasters_on_image = indices[dintandces[np.arange(dintandces.shape[0]), indices] < MEDOIDS_DISTANCE_TO_INCLUDE]
+    res = np.zeros(NROF_CLASTERS)
+    np.add.at(res, clasters_on_image, 1)
+    return res
 
-
-#r'Image filename : "VOC2005_1/PNGImages/Caltech_cars/image_0001.png"'
-#r'Image filename : "VOC2005_1/PNGImages/UIUC_TestImages_Scale/test-80.png"'
-filename_pattern = re.compile(r'Image filename : "VOC2005_\d/([/.\-_a-zA-Z0-9]*)"')
-#r'Original label for object 1 "PAScarRear" : "carRear"'
-obj_label_pattern = re.compile(r'Original label for object \d+ "(\w*)" : "(\w+)"')
-#r'Bounding box for object 1 "PAScarRear" (Xmin, Ymin) - (Xmax, Ymax) : (41, 40) - (142, 113)'
-#r'Bounding box for object 1 "PAScarRear" (Xmin, Ymin) - (Xmax, Ymax) : (410, 183) - (479, 242)'
-bb_obj_pattern = re.compile(r'Bounding box for object \d+ "\w+" \(Xmin, Ymin\) - \(Xmax, Ymax\) : \((-?\d+), (-?\d+)\) - \((\d+), (\d+)\)')
+def binary_encoding(descriprors, claster_obj):
+    res = term_frequency_encoding(descriprors, claster_obj)
+    res[res > 0] = 1
+    return res
 
 
 CLASSES = ['car', 'person', 'bike', 'bicycle']
+filename_pattern = re.compile(r'Image filename : "VOC2005_\d/([/.\-_a-zA-Z0-9]*)"')
+obj_label_pattern = re.compile(r'Original label for object \d+ "(\w*)" : "(\w+)"')
+bb_obj_pattern = re.compile(r'Bounding box for object \d+ "\w+" \(Xmin, Ymin\) - \(Xmax, Ymax\) : \((-?\d+), (-?\d+)\) - \((\d+), (\d+)\)')
 label_matcher = re.compile(rf'.*({"|".join(CLASSES)})+.*')
-#orb = ORB(file_with_BRIEFpatch=BRIEF_PATCH)
-clastering_orb = cv2.ORB_create(nfeatures=50)
+
+
+ENCODING_FUNC = binary_encoding
+ONE_CLASS_DESCRIPTOR_COUNT = 8000
+NROF_CLASTERS = 3000
+USE_ALL_DESCRIPTORS_FOR_CLASTERING = False
+GENERATE_DESCRIPTORS = False
+MEDOIDS_DISTANCE_TO_INCLUDE = 3 # близость к медоиду
+
+clastering_orb = cv2.ORB_create()
 orb = cv2.ORB_create()
-bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
 DESCRIPTOR_DUMP_FILENAME = "descriptors_set.pickle"
+MASK_ONLY = False
 def dump_descriptors_to_file(dump_filename):
     out = {i: [] for i in CLASSES}
     names = []
@@ -51,37 +63,44 @@ def dump_descriptors_to_file(dump_filename):
             assert len(all_objs_labels) == len(objs)
             orig_image = cv2.imread(image_filename)
             grey_image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2GRAY)
-            mask = np.zeros_like(grey_image)
-            for index, labels in enumerate(all_objs_labels):
-                if label_matcher.match(labels[0]):
-                    x0, y0, x1, y1 = list(map(lambda x: max(x,0), map(int, objs[index])))
-                    if x0 > x1:
-                        x0, x1 = x1, x0
-                    if y0 > y1:
-                        y0, y1 = y1, y0
-                    mask[int(y0):int(y1)+1, int(x0):int(x1)+1] = 255
+            if MASK_ONLY:
+                mask = np.zeros_like(grey_image)
+                for index, labels in enumerate(all_objs_labels):
+                    if label_matcher.match(labels[0]):
+                        x0, y0, x1, y1 = list(map(lambda x: max(x,0), map(int, objs[index])))
+                        if x0 > x1:
+                            x0, x1 = x1, x0
+                        if y0 > y1:
+                            y0, y1 = y1, y0
+                        mask[int(y0):int(y1)+1, int(x0):int(x1)+1] = 255
 
-                    kp, descriptors = clastering_orb.detectAndCompute(grey_image, mask)
 
-                    if kp:
-                        for _class in CLASSES:
-                            if _class in labels[0]:
-                                out[_class].append(descriptors)
-                    else:
-                        continue # comment for view errors
-                        img2 = cv2.drawKeypoints(grey_image, kp, None, color=(0, 255, 0), flags=0)
-                        img2[:, :, 2] = mask
-                        print(all_objs_labels, objs)
-                        print(data)
-                        plt.imshow(img2), plt.show()
+                        kp, descriptors = clastering_orb.detectAndCompute(grey_image, mask)
+
+                        if kp:
+                            for _class in CLASSES:
+                                if _class in labels[0]:
+                                    out[_class].append(descriptors)
+                        else:
+                            continue # comment for view errors
+                            img2 = cv2.drawKeypoints(grey_image, kp, None, color=(0, 255, 0), flags=0)
+                            img2[:, :, 2] = mask
+                            print(all_objs_labels, objs)
+                            print(data)
+                            plt.imshow(img2), plt.show()
+            else:
+                kp, descriptors = clastering_orb.detectAndCompute(grey_image, None)
+
+                if kp:
+                    out[CLASSES[np.random.randint(len(CLASSES))]].append(descriptors)
 
             #show_image(orig_image)
         except Exception as e:
             logger.exception(e)
     print("\nDescriptor counts:")
     for _class in CLASSES:
-        descriptors[_class] = np.vstack(descriptors[_class])
-        print(f"\t{_class}: {descriptors[_class].shape[0]}")
+        out[_class] = np.vstack(out[_class])
+        print(f"\t{_class}: {out[_class].shape[0]}")
 
     import pickle
     with open(dump_filename, "wb") as f:
@@ -89,22 +108,18 @@ def dump_descriptors_to_file(dump_filename):
     print(f"descriptors was dumped to {dump_filename}")
 
 
-ONE_CLASS_DESCRIPTOR_COUNT = 9000
-NROF_CLASTERS = 5000
-ALL = False
-THRESHOLD = 100 # близость к медоиду
 def k_medoids():
     import pickle
     with open(DESCRIPTOR_DUMP_FILENAME, 'rb') as f:
         descriptors = pickle.load(f)
-    print("\nDescriptor counts:")
+    print("\nDescriptors counts:")
     for _class in CLASSES:
         print(f"\t{_class}: {descriptors[_class].shape[0]}")
 
 
     claster_desctiptors = []
     for _class in CLASSES:
-        if ALL:
+        if USE_ALL_DESCRIPTORS_FOR_CLASTERING:
             claster_desctiptors.append(descriptors[_class])
         else:
             claster_desctiptors.append(descriptors[_class]\
@@ -116,23 +131,7 @@ def k_medoids():
 
     kmedoids_obj = KMedoids(n_clusters=NROF_CLASTERS, metric='hamming', init="k-medoids++", max_iter=300).fit(claster_desctiptors)
     return kmedoids_obj
-'''
-    claster_objs = []
-    claster_per_class = NROF_CLASTERS // (len(CLASSES))
-    for _class in CLASSES:
 
-        #claster_desctiptors.append(descriptors[_class])
-
-        claster_desctiptors = np.unpackbits(descriptors[_class], axis=1)
-        print(f"descriptors shape for clastering = {claster_desctiptors.shape}")
-
-        kmedoids_obj = KMedoids(n_clusters=claster_per_class, metric='hamming', init="k-medoids++", max_iter=300).fit(
-            claster_desctiptors)
-        claster_objs.append(kmedoids_obj)
-
-
-    return claster_objs
-'''
 
 class MLP(nn.Module):
     def __init__(self):
@@ -148,37 +147,12 @@ class MLP(nn.Module):
         return x
 
 
-def term_frequency_encoding(descriptors, medoids_obj):
-
-    dintandces = medoids_obj.transform(descriptors)
-    indices = np.argmin(dintandces, axis=1)
-    clasters_on_image = indices[dintandces[np.arange(dintandces.shape[0]), indices] < THRESHOLD]
-    res = np.zeros(NROF_CLASTERS)
-    np.add.at(res, clasters_on_image, 1)
-    return res
-'''
-    out = []
-    for med in medoids_obj:
-        dintandces = med.transform(descriptors)
-        indices = np.argmin(dintandces, axis=1)
-        clasters_on_image = indices[dintandces[np.arange(dintandces.shape[0]), indices] < THRESHOLD]
-        res = np.zeros(NROF_CLASTERS // len(CLASSES))
-        np.add.at(res, clasters_on_image, 1)
-        out.append(res)
-    out = np.concatenate(out)
-    return out
-'''
-
-def binary_encoding(descriprors, medoids_obj):
-    res = term_frequency_encoding(descriprors, medoids_obj)
-    res[res > 0] = 1
-    return res
-
 class VOCDataset(Dataset):
     def __init__(self,voc_path, medoids_object, encoding_func):
-        dirs_by_class = {i: [] for i in CLASSES}
         self.samples = []
-        temp_y_vector = np.zeros(len(CLASSES))
+
+        '''
+        dirs_by_class = {i: [] for i in CLASSES}
         dirs = [os_path.join(voc_path, "PNGImages", i) for i in listdir(os_path.join(voc_path, "PNGImages"))]
 
         for dir in dirs:
@@ -187,14 +161,47 @@ class VOCDataset(Dataset):
                     dirs_by_class[_class].append(dir)
 
         for index, _class in enumerate(CLASSES):
-            y_t = temp_y_vector[:]
-            y_t[index] = 1
             for dir in dirs_by_class[_class]:
                 for image_filename in listdir(dir):
                     image = cv2.imread(os_path.join(dir, image_filename))
                     kp, descriptors = clastering_orb.detectAndCompute(image, None)
-                    x_t = encoding_func(np.unpackbits(descriptors, axis=1), medoids_object)
+                    x_t = encoding_func(descriptors, medoids_object)
                     self.samples.append((torch.tensor(x_t).float(), index))
+        '''
+        names = []
+        dirs = [os_path.join(voc_path, "Annotations", i) for i in
+                listdir(os_path.join(voc_path, "Annotations"))]
+        for dir in dirs:
+            names += [os_path.join(dir, i) for i in listdir(dir)]
+
+        names = names
+        im_count = len(names)
+        for im_index, name in enumerate(names):
+            try:
+                print(f"\rhandling {im_index}/{im_count}: {name}...", end='')
+                with open(name) as f:
+                    data = f.read()
+                image_filename = os_path.join(voc_path, filename_pattern.findall(data)[0])
+                all_objs_labels = obj_label_pattern.findall(data)
+
+                orig_image = cv2.imread(image_filename)
+                grey_image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2GRAY)
+
+                classes = []
+                for index, labels in enumerate(all_objs_labels):
+                    for _class in CLASSES:
+                        if _class in labels[0] and _class not in classes:
+                            classes.append(_class)
+
+                if len(classes) == 1:
+                    kp, descriptors = clastering_orb.detectAndCompute(grey_image, None)
+
+                    if kp:
+                        x = encoding_func(descriptors, medoid_obj)
+                        self.samples.append((torch.tensor(x).float(), CLASSES.index(classes[0])))
+            except Exception as e:
+                logger.exception(e)
+        print(f"VOC SIZE of {voc_path} is {len(self.samples)}")
 
     def __len__(self):
         return len(self.samples)
@@ -204,25 +211,27 @@ class VOCDataset(Dataset):
 
 
 
-def learn_MLP(medoids_obj):
-    train_dataset = VOCDataset(VOC_TRAIN_PATH, medoids_obj, term_frequency_encoding)
-    test_dataset = VOCDataset(VOC_TEST_PATH, medoids_obj, term_frequency_encoding)
+def learn_MLP(medoids_obj, encoding_func):
+    train_dataset = VOCDataset(VOC_TRAIN_PATH, medoids_obj, encoding_func)
+    test_dataset = VOCDataset(VOC_TEST_PATH, medoids_obj, encoding_func)
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=False)
     model = MLP()
     print(model)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    print(f"Train dataset size: {len(train_dataset)}")
+    print(f"Test dataset size: {len(test_dataset)}")
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     loss_fn = nn.CrossEntropyLoss()
     mean_train_losses = []
-    mean_valid_losses = []
-    valid_acc_list = []
-    epochs = 15
+    mean_test_losses = []
+    test_acc_list = []
+    epochs = 30
 
     for epoch in range(epochs):
         model.train()
 
         train_losses = []
-        valid_losses = []
+        test_losses = []
         for i, (images, labels) in enumerate(train_loader):
 
             optimizer.zero_grad()
@@ -242,35 +251,36 @@ def learn_MLP(medoids_obj):
                 outputs = model(images)
                 loss = loss_fn(outputs, labels)
 
-                valid_losses.append(loss.item())
+                test_losses.append(loss.item())
 
                 _, predicted = torch.max(outputs.data, 1)
                 correct += (predicted == labels).sum().item()
                 total += labels.size(0)
 
         mean_train_losses.append(np.mean(train_losses))
-        mean_valid_losses.append(np.mean(valid_losses))
+        mean_test_losses.append(np.mean(test_losses))
 
         accuracy = 100 * correct / total
-        valid_acc_list.append(accuracy)
+        test_acc_list.append(accuracy)
         print('epoch : {}, train loss : {:.4f}, test loss : {:.4f}, test acc : {:.2f}%' \
-              .format(epoch + 1, np.mean(train_losses), np.mean(valid_losses), accuracy))
+              .format(epoch + 1, np.mean(train_losses), np.mean(test_losses), accuracy))
 
     fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(15, 10))
     ax1.plot(mean_train_losses, label='train')
-    ax1.plot(mean_valid_losses, label='valid')
+    ax1.plot(mean_test_losses, label='test')
     lines, labels = ax1.get_legend_handles_labels()
     ax1.legend(lines, labels, loc='best')
 
-    ax2.plot(valid_acc_list, label='valid acc')
+    ax2.plot(test_acc_list, label='test acc')
     ax2.legend()
     plt.show()
 
 
 
 if __name__=="__main__":
-    #dump_descriptors_to_file(DESCRIPTOR_DUMP_FILENAME)
+    if GENERATE_DESCRIPTORS:
+        dump_descriptors_to_file(DESCRIPTOR_DUMP_FILENAME)
     medoid_obj = k_medoids()
-    learn_MLP(medoid_obj)
+    learn_MLP(medoid_obj, ENCODING_FUNC)
 
 
--- object_localization.py
+++ object_localization.py
@@ -4,19 +4,81 @@ from matplotlib import pyplot as plt
 OBJ_PATH = r'box.png'
 SCENE_PATH = r'box_in_scene.png'
 
+
 class Localizator():
     def __init__(self):
-        self.orb = cv2.ORB_create()
+        self.scale_factor = 1.2
+        self.nlevels = 8
+        self.angle_step = 12
+        self.scene_y_step = 10
+        self.scene_x_step = 10
+        self.match_count = 5
+        self.orb = cv2.ORB_create(scaleFactor=self.scale_factor, nlevels=self.nlevels)
 
     def __call__(self, object, scene):
+        scene_H, scene_W = scene.shape
+        obj_H, obj_W = object.shape
+        obj_center = (obj_W / 2, obj_H / 2)
         kp1, des1 = self.orb.detectAndCompute(object, None)
         kp2, des2 = self.orb.detectAndCompute(scene, None)
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         matches = bf.match(des1, des2)
-        matches = sorted(matches, key=lambda x: x.distance)
+        matches = sorted(matches, key=lambda x: x.distance) #[:self.match_count]
 
-        match_img = cv2.drawMatches(object, kp1, scene, kp2, matches[:10], None)
-        plt.imshow(match_img), plt.show()
+        accum = [[[[[] for _ in range(0, scene_W, self.scene_x_step)]
+                   for _ in range(0, scene_H, self.scene_y_step)]
+                  for _ in range(0, 361, self.angle_step)]
+                 for _ in range(-self.nlevels, self.nlevels + 1)]
+
+        max_len = 0
+        max_features = 0
+        max_keypoints = None
+        print(scene_H, scene_W)
+        for match in matches:
+            obj_kp = kp1[match.queryIdx]
+            scene_kp = kp2[match.trainIdx]
+            delta_theta = scene_kp.angle - obj_kp.angle
+            scale = scene_kp.octave - obj_kp.octave
+            center_vec = obj_center[0] - obj_kp.pt[0], obj_center[1] - obj_kp.pt[1]
+            cv2.line(object, (int(obj_kp.pt[0]), int(obj_kp.pt[1])),
+                     (int(obj_kp.pt[0] + center_vec[0]), int(obj_kp.pt[1] + center_vec[1])), 255, 1)
+            center_vec = (
+                np.array(center_vec).dot(
+                    cv2.getRotationMatrix2D(center=(0, 0), angle=delta_theta,
+                                            scale=(self.scale_factor ** scale))[:, :2]))
+            cv2.line(scene, (int(scene_kp.pt[0]), int(scene_kp.pt[1])),
+                     (int(scene_kp.pt[0] + center_vec[0]), int(scene_kp.pt[1] + center_vec[1])), 255, 1)
+
+
+            features = (scale, int(delta_theta // self.angle_step), int((scene_kp.pt[1] + center_vec[1]) // self.scene_y_step),
+                        int((scene_kp.pt[0] + center_vec[0]) // self.scene_x_step))
+            print(features)
+            try:
+                curr_list = accum[features[0]][features[1]][features[2]][features[3]]
+                # noinspection PyTypeChecker
+                curr_list.append((obj_kp, scene_kp))
+                if len(curr_list) > max_len:
+                    max_keypoints = curr_list
+                    max_features = features
+                    max_len = len(curr_list)
+            except IndexError as e:
+                continue # bad match?? point + center_vec
+
+        print(max_features, max_len)
+        center = np.array([max_features[3] * self.scene_x_step+self.scene_x_step//2, max_features[2]* self.scene_y_step + self.scene_y_step//2])
+        rect_kp = np.array([(-obj_W//2, -obj_H//2), (-obj_W//2, obj_H//2), (obj_W//2, obj_H//2), (obj_W//2, -obj_H//2)])
+        rect_new = rect_kp.dot(cv2.getRotationMatrix2D(center=(0, 0), angle=(max_features[1]*self.angle_step + self.angle_step/2),
+                                            scale=(self.scale_factor ** max_features[0]))[:, :2])
+        rect_new = np.int0(rect_new + center)
+        
+        scene = cv2.cvtColor(scene, cv2.COLOR_GRAY2BGR)
+        cv2.drawContours(scene, [rect_new], 0, (0, 255, 0), 2)
+
+
+
+        img = cv2.drawMatches(object, kp1, scene, kp2, matches, None)
+        plt.imshow(img), plt.show()
+
 
 
 if __name__ == "__main__":
@@ -30,5 +92,3 @@ if __name__ == "__main__":
 
     orb = cv2.ORB_create()
     kp1, des1 = orb.detectAndCompute(grey_obj, None)
-
-
